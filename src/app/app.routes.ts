8930c540a85703fc9b4a80d7dab969c8ad6d37de
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-04-21 16:06:03 +0530

    Update routing configuration to use lazy loading for HomeComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
   {
     path: 'with-signal',
     loadComponent: () =>
